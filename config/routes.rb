045c1ecd503a7a9b5eeb10faef4dca0984a7f50b
Yuji Hosokai <uugo.hosokai@gmail.com>
2020-04-19 20:32:52 +0900

    devise/MVCの作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,16 @@ Rails.application.routes.draw do
   get 'home/index'
   get 'home/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  devise_for :users, :controllers => {
+  :registrations => 'users/registrations',
+  :sessions => 'users/sessions'   
+} 
+
+devise_scope :user do
+  get "user/:id", :to => "users/registrations#detail"
+  get "signup", :to => "users/registrations#new"
+  get "login", :to => "users/sessions#new"
+  get "logout", :to => "users/sessions#destroy"
+end
 end
