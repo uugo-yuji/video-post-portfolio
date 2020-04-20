13cbae52e4859808894ecb5d278c5e59dbf5ad2d
Yuji Hosokai <uugo.hosokai@gmail.com>
2020-04-20 20:48:05 +0900

    #1 RSpecでのテスト

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ module VideoPost
     config.generators do |g|
       g.test_framework :rspec,
       view_specs: false, 
-      helper_specs: false,
+      helper_specs: false
     end
   end
 end
