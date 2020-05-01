f0a335447c64c611f19ea445a34d192436e754aa
Yuji Hosokai <uugo.hosokai@gmail.com>
2020-05-01 17:00:58 +0900

    postmodel,postcontrollerの作成

--- config/routes.rb
+++ config/routes.rb
@@ -8,10 +8,12 @@ Rails.application.routes.draw do
   :sessions => 'users/sessions'   
 } 
 
-devise_scope :user do
-  get "user/:id", :to => "users/registrations#detail"
-  get "signup", :to => "users/registrations#new"
-  get "login", :to => "users/sessions#new"
-  get "logout", :to => "users/sessions#destroy"
-end
+  devise_scope :user do
+    get "user/:id", :to => "users/registrations#detail"
+    get "signup", :to => "users/registrations#new"
+    get "login", :to => "users/sessions#new"
+    get "logout", :to => "users/sessions#destroy"
+  end
+
+  resources :posts
 end
---
+++ spec/factories/posts.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :post do
+    title { "MyString" }
+    price { "MyString" }
+    content { "MyString" }
+    video { "MyString" }
+    user { nil }
+  end
+end
