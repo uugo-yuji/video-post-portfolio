132ece9ca0fdd5b1cd9017039589c6315f541b71
Yuji Hosokai <uugo.hosokai@gmail.com>
2020-05-03 21:51:48 +0900

    タグの検索機能（簡易版）

--- app/models/post.rb
+++ app/models/post.rb
@@ -22,7 +22,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
-  has_many :post_tag_relations
+  has_many :post_tag_relations, dependent: :delete_all
   has_many :tags, through: :post_tag_relations
   
   validates :title, presence: true, length: { maximum: 30}
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -8,6 +8,6 @@
 #  updated_at :datetime         not null
 #
 class Tag < ApplicationRecord
-  has_many :post_tag_relations
+  has_many :post_tag_relations, dependent: :delete_all
   has_many :posts, through: :post_tag_relations
 end
