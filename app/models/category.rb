328fa803efbc2a76d4cfcd032a4d991488293f59
Yuji Hosokai <uugo.hosokai@gmail.com>
2020-05-13 01:50:01 +0900

    tagからcategoryへの名称変更。影響範囲への修正（動作確認済）/ fix#21

---
+++ app/models/category.rb
@@ -0,0 +1,13 @@
+# == Schema Information
+#
+# Table name: categories
+#
+#  id         :bigint           not null, primary key
+#  name       :string(255)      not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class Category < ApplicationRecord
+  has_many :post_category_relations
+  has_many :posts, through: :post_category_relations
+end
