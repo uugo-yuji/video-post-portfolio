2f8d23a4aa23607bfcd974776a593077fba4187e
Yuji Hosokai <uugo.hosokai@gmail.com>
2020-05-20 18:33:27 +0900

    ホーム,投稿一覧,headerレイアウトの変更  #19

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,10 @@ class HomeController < ApplicationController
   before_action :sign_in_required, only: [:show]
 
   def index
+    @posts = params[:category_id].present? ? Category.find(params[:category_id]).posts : Post.all.order(created_at: :desc)
+    @posts = @posts.page(params[:page])
+    @categories = Category.all
+    @bookmark = Bookmark.new
   end
 
   def show
