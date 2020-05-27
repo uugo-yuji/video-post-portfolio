b0affda32f4a4147c113f37c04cd717eeaa040a8
Yuji Hosokai <uugo.hosokai@gmail.com>
2020-05-28 01:19:57 +0900

    postモデルRSpecテスト #32

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -23,5 +23,23 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  # 有効なファクトリを持つこと
+  it "has a valid factory" do
+    expect(FactoryBot.build(:post)).to be_valid
+  end
+
+    # タイトルが30文字以上だと無効なこと
+    it "is invalid with a more title" do
+      post = build(:post, title: 'a' * 31)
+      post.valid?
+      expect(post.errors[:title][0]).to include("は30文字以内で入力してください")
+    end
+
+    # 本文が1000文字以上だと無効なこと
+    it "is invalid with a more content" do
+      post = build(:post, content: 'a' * 1001)
+      post.valid?
+      expect(post.errors[:content][0]).to include("")
+    end
 end
