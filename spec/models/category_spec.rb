52ebf6895e3483a8c85a6e67b32d6d889f30d83b
Yuji Hosokai <uugo.hosokai@gmail.com>
2020-05-28 12:26:47 +0900

    カテゴリモデルRspecTest #34 fix #34

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -10,5 +10,24 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:category) { FactoryBot.create(:category) }
+
+    # 有効なファクトリを持つこと
+    it "has a valid factory" do
+      expect(FactoryBot.build(:category)).to be_valid
+    end
+
+    # 名前がなければ無効なこと
+    it "is invalid without an name" do
+      category = FactoryBot.build(:category, name: nil)
+      category.valid?
+      expect(category.errors.added?(:name, :blank)).to be_truthy
+    end
+
+    # カテゴリが30以上だと無効なこと
+    it "is invalid without a a more name" do
+      category = FactoryBot.build(:category, name: 'a' * 31)
+      category.valid?
+      expect(category.errors[:name]).to include("は30文字以内で入力してください")
+    end
 end
